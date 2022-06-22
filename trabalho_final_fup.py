a16d970aec581ac9e89dc9de655be597cadcefb5
Enzo Esmeraldo <75813033+enso625@users.noreply.github.com>
2022-06-22 17:47:15 -0300

    Add files via upload

---
+++ trabalho_final_fup.py
@@ -0,0 +1,14 @@
+# Enzo -------------------
+# função para criaçao de matriz
+def começo(m):
+    matriz = [m*[0], m*[0], m*[0], m*[0]]
+    for i in range(0, m):
+        for j in range(0, m):
+            matriz[i][j] = int(input(f"Digite o elemento [{i}, {j}]: "))
+    return matriz
+
+
+# Bruno ------------------
+
+
+# Julia ------------------

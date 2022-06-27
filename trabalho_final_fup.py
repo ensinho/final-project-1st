01ffa580c9be8a96eee6a73be3bbb701375b4457
Enzo Esmeraldo <75813033+enso625@users.noreply.github.com>
2022-06-27 15:17:02 -0300

    Update trabalho_final_fup.py

--- trabalho_final_fup.py
+++ trabalho_final_fup.py
@@ -1,19 +1,25 @@
-# Enzo -------------------
-# função para criaçao de matriz
+# <<<<<<<<<<<<<<<<<<<<<<< Enzo >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# <--- função para criação de matriz --->
+
 def começo(m):
     matriz = [m*[0], m*[0], m*[0], m*[0]]
+
     for i in range(0, m):
         for j in range(0, m):
             matriz[i][j] = int(input(f"Digite o elemento [{i}, {j}]: "))
+
     return matriz
 
+
 # <--- Escrita dos arquivos - paralelo à criação da função --->
+
 def abrir_arq(x):
     arquivo = open("Tamanho_peças", "a")
 
     for i in range(qtd_peças):
         tamanho = input("Qual o tamanho da peça? ")
-        tam = (f" \n Tamanho da peça [{i+1}]: {tamanho} ")
+        tam = (f" Tamanho da peça [{i+1}]: {tamanho}\n")
 
         arquivo.write(tam)
 
@@ -23,7 +29,8 @@ def abrir_arq(x):
 qtd_peças = int(input("Quantas peças vão ser adicionadas? "))
 arq = abrir_arq(qtd_peças)
 
-# <----- Leitura dos dados inseridos no arquivo -----> 
+# <----- Leitura dos dados inseridos no arquivo ----->
+
 arquivo = open("Tamanho_peças", "r")
 
 conteudo = arquivo.read()
@@ -31,7 +38,31 @@ print(conteudo)
 
 arquivo.close()
 
-# Bruno ------------------
+# <--- Criação de listas a partir do arquivo --->
+
+arquivo = open("Tamanho_peças", "r")
+
+tamanhos = arquivo.readlines()
+
+print(tamanhos)
 
 
-# Julia ------------------
+# <--- função de remoção de linhas --->
+
+def remov_linha(num_linhas, nome_arq):
+    arquivo = open(nome_arq, "r")
+
+    linhas = arquivo.readlines()
+
+    arquivo.close()
+
+    del(linhas[num_linhas-1])
+
+    arquivo = open(nome_arq, "w")
+    arquivo.writelines(linhas)
+
+
+
+# <<<<<<<<<<<<<<<<<<<<<<< Bruno >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+# <<<<<<<<<<<<<<<<<<<<<<< Julia >>>>>>>>>>>>>>>>>>>>>>>>>>>

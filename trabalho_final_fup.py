6df4cd30c68ee551100699af1e4442ae8227e15c
Enzo Esmeraldo <75813033+ensinho@users.noreply.github.com>
2022-07-04 09:42:25 -0300

    Update trabalho_final_fup.py

--- trabalho_final_fup.py
+++ trabalho_final_fup.py
@@ -1,14 +1,14 @@
 # <<<<<<<<<<<<<<<<<<<<<<< Enzo >>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+#função para reniciar o programa
 import sys
 import os
-
 
 def restart_program():
     python = sys.executable
     os.execl(python, python, * sys.argv)
 
 # <--- função para criação de matriz --->
-
 
 def criar_matriz(n, m):
     m1 = []
@@ -20,69 +20,57 @@ def criar_matriz(n, m):
         m1.append(linha)
     return m1
 
-# <--- Escrita dos arquivos - paralelo à criação da função --->
-
-# def abrir_arq(a):
-    # arquivo = open("Tamanho_peças", "a")
-
-    # for i in range(qtd_peças):
-    # tamanho = input("Qual o tamanho da peça? ")
-    # tam = (f" Tamanho da peça [{i+1}]: {tamanho}\n")
-
-    # arquivo.write(tam)
-
-    # arquivo.close()
-
-# <--- criação de arquivos generalizada --->
+# <--- função principal, de adicionar peças --->
 
 
 def arq_abrir(quantidade):
+    tag = 1
+    
     arquivo = open("Armario", "a", encoding="utf-8")
-    for i in range(quantidade):
-        tag = int(input("Qual o ID do item? "))
+
+    for i in range(quantidade): 
+
+        #tag = int(input("Qual o ID do item? "))
+
         tipo = input(f"Qual o tipo do item [{tag}] ? ")
+
         sexo = input("Qual o gênero deste item? ")
+
         tamanho = input("Qual o tamanho do item?")
+        while tamanho != "p" and tamanho != "m" and tamanho != "g":
+            tamanho = input("Qual o tamanho do item?")
         color = input("Qual a cor ?")
+
         estilo = input("Qual o estilo da peça? ")
+
         date = int(input(("Qual a data de compra deste item? ")))
+
         stats = input("Qual o status desse item? [Venda] [Doação] [Manter] ")
         if stats != "Venda":
             price = 0
         else:
             price = int(input("Qual o preço deste item?"))
+
+        tag += 1
+
         arquivo.write(
-            f"Id:[{tag}] | tipo:[{tipo}] | sexo:[{sexo}] | tamanho:[{tamanho}] | cor:[{color}] | data de compra:[{date}] | status:[{stats}] | preço:[{price}]| estilo:[{estilo}]")
+            f"Id:[{tag}] | tipo:[{tipo}] | sexo:[{sexo}] | tamanho:[{tamanho}] | cor:[{color}] | data de compra:[{date}] | status:[{stats}] | preço:[{price}]| estilo:[{estilo}]\n")
 
     arquivo.close()
-
-
-# qtd_peças = int(input("Quantas peças vão ser adicionadas? "))
-# arq = abrir_arq(qtd_peças)'''
-
-
-# <--- Criação de listas a partir do arquivo --->
-
-# arquivo = open("Tamanho_peças", "r")
-
-# tamanhos = arquivo.readlines()
-
-# print(tamanhos)
-
 
 # <--- função de remoção de linhas --->
 
 def remov_linha(num_linhas):
     arquivo = open("Armario", "r")
 
-    linhas = arquivo.readlines()
+    linhasr = arquivo.readlines()
 
     arquivo.close()
 
-    del(linhas[num_linhas-1])
+    del(linhasr[num_linhas-1])
 
     arquivo = open("Armario", "w")
-    arquivo.writelines(linhas)
+    arquivo.writelines(linhasr)
 
 # <--- função de alteração das linhas --->
 
@@ -93,28 +81,53 @@ def alterar_linhas(num_linhas, novo):
     linhas = arquivo.readlines()
 
     arquivo.close()
+    
+    linhas.insert(num_linhas-1, novo)
 
-    linhas.insert((num_linhas-1), novo)
+    del(linhas[num_linhas])
 
-    del(linhas[num_linhas - 1])
+    linhas.remove(None)
 
     arquivo = open("Armario", "w")
     arquivo.writelines(linhas)
 
 # <--- adição dos elementos em um arquivo --->
 
+opcoes = [" < ------ Á R M A R I O ------ >"  , "Digite [1] para adicionar novas peças! " ,"Digite [2] para visualizar suas peças. " , "Digite [3] para alterar alguma peça." , "Digite [4] para remover alguma peça. " , "Digite [5] para sair." ]
 
-print(
-    " < ------ Á R M A R I O ------ >   \n Digite [Adicionar] para novas peças! [Visualizar] para ver suas peças. \n [Alterar] para alterar alguma peça. [Remover] para remover alguma peça.")
-iniciar = input("Deseja tomar alguma ação? ")
-# essa ideia ainda nao está pronta, mas seria um menu para selecioar as açoes
-if iniciar == "Adicionar":
+for o in range(6):
+    print(opcoes[o])
 
-    # nao pedir o nome do arquivo, ja incrementar num arquivo chamado Armario.
+
+while True:
+    iniciar = int(input("Qual ação irá ser tomada? "))
+
+    if iniciar > 5 or iniciar < 1:
+        print("Entrada inválida! Tente novamente com outro número. ")
+    else:
+        break
+
+
+if iniciar == 1:
+
     quantidade = int(input("Quantos itens serão adicionados? "))
+    while quantidade <= 0:
+        print("Entrada Inválida, tente novamente!")
+        quantidade = int(input("Quantos itens serão adicionados? "))
+
     teste = arq_abrir(quantidade)
 
-if iniciar == "Visualizar":
+    while True:
+        continuar = input("Deseja adicionar uma outra peça? ")
+        if continuar == "sim":
+            teste1 = arq_abrir(1)
+        elif continuar == "nao":
+            fechar = input("Dseja fechar o programa? ")
+            if fechar == "sim":
+                break
+            exit()
+
+if iniciar == 2:
     # <----- Leitura dos dados inseridos no arquivo ----->
 
     arquivo = open("Armario", "r")
@@ -130,16 +143,44 @@ if iniciar == "Visualizar":
     else:
         restart_program()
 
-# agumas funçoes, como essa ainda nao estao funcionando devidamente
-# função alterar nao esta alterando, apenas adicionando uma nova peça
 
-if iniciar == "Alterar":
+if iniciar == 3:
     item = int(input("Qual linha você deseja alterar? "))
     novo = arq_abrir(1)
+
     alteraçao = alterar_linhas(item, novo)
-if iniciar == "Remover":
-    onde = int(input("QUal linha você deseja remover? "))
+
+    alt_novo = input("Deseja alterar alguma outra peça? ")
+
+    if alt_novo == "sim":
+        item1 = int(input("Qual linha você deseja alterar? "))
+        novo1 = arq_abrir(1)
+        alteraçao1 = alterar_linhas(item1, novo1)
+
+    else:
+        fim = input("Deseja sair do guarda-roupa? ")
+        if fim == "sim":
+            exit()
+        else:
+            restart_program()
+
+if iniciar == 4:
+    onde = int(input("Qual linha você deseja remover? "))
     remoçao = remov_linha(onde)
+
+    remov_novo = input("Deseja remover outra linha? ")
+    if remov_novo == "sim":
+        onde1 = int(input("Qual linha você deseja remover? "))
+        remoçao1 = remov_linha(onde1)
+    else: 
+        fim = input("Deseja sair do guarda-roupa? ")
+        if fim == "sim":
+            exit()
+        else:
+            restart_program()
+
+if iniciar == 5:
+    exit()
 
 
 # <<<<<<<<<<<<<<<<<<<<<<< Bruno >>>>>>>>>>>>>>>>>>>>>>>>>>>
